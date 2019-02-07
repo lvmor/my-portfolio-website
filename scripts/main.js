0c9dc8b220f5723770d64aff8be88929db65a04d
lvmor <lvm.lspa@gmail.com>
2019-02-07 15:44:51 -0800

    added carousel sample image for 1 buttone

--- scripts/main.js
+++ scripts/main.js
@@ -4,11 +4,22 @@ $(document).ready(function() {
   // console.log('test')
   $("button").on("click", function(){
     console.log("it works");
+    console.log(project[0]);
+    $(".content").html(`<div>${project[0]}</div>`);
+    $(".projImg").attr("src", "images/sample-img.jpg");
+
     for(let i=0; i < project.length; i++){
       console.log(project[i]);
       console.log("loop works");
+      // document.write(project[i] + "</br>");
+      $("")
     };
 });
 
 });
 
+// $("#box1").on("click", function () {
+// 	$(".moto").attr("src", "images/moto.jpg");
+
+
+// $(".content").append(`<div>${project[0]}</div>`);
