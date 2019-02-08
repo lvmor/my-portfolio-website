ab81394ffef9e06924aad231ce26e133003ac477
lvmor <lvm.lspa@gmail.com>
2019-02-07 21:54:47 -0800

    created HTML general structure

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,26 @@
-var project = ["images/sample-img1.jpg", "images/sample-img2.jpg", "images/sample-img3.jpg"];
+var projectImg = ["images/sample-img1.jpg", "images/sample-img2.jpg", "images/sample-img3.jpg"];
 var currentTimerID = null;
 
 $(document).ready(function(){
-$(".projImg").attr("src", "images/sample-img1.jpg");
-	$(".btn-1").on("click", function(){
-		$(".projImg").attr("src", "images/sample-img1.jpg");
+// $(".projImg").attr("src", "images/sample-img1.jpg");
+$(".projImg").attr("src", projectImg[0]);
+	$(".button").on("click", function(){
+    // for(let i=0; i < project.length; i++){
+      if(currentTimerID === null){
+        currentTimerID = setInterval(projectImg[1], 1000);
+      
+      }
+
+    // }
+
+
 	})//end of button 1 func
-	$(".btn-2").on("click", function(){
-		$(".projImg").attr("src", "images/sample-img2.jpg");
-	})//end of button 2 func
-	$(".btn-3").on("click", function(){
-		$(".projImg").attr("src", "images/sample-img3.jpg");
-	})//end of button 3 func
+	// $(".btn-2").on("click", function(){
+	// 	$(".projImg").attr("src", "images/sample-img2.jpg");
+	// })//end of button 2 func
+	// $(".btn-3").on("click", function(){
+	// 	$(".projImg").attr("src", "images/sample-img3.jpg");
+	// })//end of button 3 func
 })//end of ready func
 
 
@@ -23,7 +32,16 @@ $(".projImg").attr("src", "images/sample-img1.jpg");
     // };
 // $(".content").append(`<div>${project[0]}</div>`);
 
+$("#start").click(function(){
+  if(currentTimerID === null){
+    currentTimerID = setInterval(projectImg, 1000);
+  }//end of if
+})// end of start
 
+$("#stop").click(function(){
+  clearInterval(currentTimerID);
+  currentTimerID = null;
+})
 
 function sayHi(){
   console.log("test");
