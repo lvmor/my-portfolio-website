4ed2ac8da1166cd24c8e4ef4225202fab1781355
lvmor <lvm.lspa@gmail.com>
2019-02-07 16:45:36 -0800

    added 3 images and 3 buttons with interval function

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,41 @@
-var project = [1, 2, 3];
+var project = ["images/sample-img1.jpg", "images/sample-img2.jpg", "images/sample-img3.jpg"];
+var currentTimerID = null;
 
-$(document).ready(function() {
-  // console.log('test')
-  $("button").on("click", function(){
-    console.log("it works");
-    console.log(project[0]);
-    $(".content").html(`<div>${project[0]}</div>`);
-    $(".projImg").attr("src", "images/sample-img.jpg");
-
-    for(let i=0; i < project.length; i++){
-      console.log(project[i]);
-      console.log("loop works");
-      // document.write(project[i] + "</br>");
-      $("")
-    };
-});
-
-});
-
-// $("#box1").on("click", function () {
-// 	$(".moto").attr("src", "images/moto.jpg");
+$(document).ready(function(){
+$(".projImg").attr("src", "images/sample-img1.jpg");
+	$(".btn-1").on("click", function(){
+		$(".projImg").attr("src", "images/sample-img1.jpg");
+	})//end of button 1 func
+	$(".btn-2").on("click", function(){
+		$(".projImg").attr("src", "images/sample-img2.jpg");
+	})//end of button 2 func
+	$(".btn-3").on("click", function(){
+		$(".projImg").attr("src", "images/sample-img3.jpg");
+	})//end of button 3 func
+})//end of ready func
 
 
+    // for(let i=0; i < project.length; i++){
+    //   console.log(project[i]);
+    //   console.log("loop works");
+    //   // document.write(project[i] + "</br>");
+    //   $("")
+    // };
 // $(".content").append(`<div>${project[0]}</div>`);
+
+
+
+function sayHi(){
+  console.log("test");
+}  if(currentTimerID === null){
+  currentTimerID = setInterval(sayHi, 1000);
+}
+$("#start").click(function(){
+  if(currentTimerID === null){
+    currentTimerID = setInterval(sayHi, 1000);
+  }
+});
+$("#stop").click(function(){
+  clearInterval(currentTimerID);
+  currentTimerID = null;
+});
