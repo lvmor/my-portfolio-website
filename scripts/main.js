8c05dd7ddb99f4d9c649eb083be257fbe2f73381
lvmor <lvm.lspa@gmail.com>
2019-02-10 08:47:00 -0800

    added images, modified nav bar

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
-let projectImg = ["images/sample-img1.jpg", "images/sample-img2.jpg", "images/sample-img3.jpg"];
+// Make images change at an interval
+// //https://www.youtube.com/watch?v=4YQ4svkETS0&t=211s
+let projectImg = ["images/img1.jpeg", "images/img2.jpeg", "images/img3.jpeg"];
 const time = 3000;
 let i = 0
 
@@ -14,34 +16,20 @@ function imgChange(){
 
 window.onload = imgChange;
 
-//https://www.youtube.com/watch?v=4YQ4svkETS0&t=211s
+//user can click next or prev to navigate to a particular project
 
-
-//carousel image to enlarge in a lightbox when the user clicks on it
-
-$(".projImg").on("click", function (){
-  if($(this).width() == 500){
-    $(this).width(214);
-  } else {
-    $(this).width(500); //asign variable for emsize
-  }
-});
-
-
-//clicking anywhere on the screen when the lightbox is open should close the lightbox.
-function handlePrevious(){
+$("#previous").on("click", function(){
   i--;
-  if(i == -1) {
+  if(i === -1) {
     i = projectImg.length - 1;
   }
   console.log('previous clicked', i);
   showImage();
-}
-$("#previous").on("click", handlePrevious);
+});
 
 $("#next").on("click", function (){
   i++;
-  if(i == projectImg.length){
+  if(i === projectImg.length){
     i =  0;
   }
   console.log('next clicked', i);
@@ -52,6 +40,56 @@ function showImage() {
   document.slide.src = projectImg[i];
 }
 
+//carousel image to enlarge in a lightbox when the user clicks on it
+
+$(".projImg").on("click", function (){
+  modal.style.display = "block";
+});
+
+$(window).on("click", function (){
+  modal.style.display = "none";
+})
+
+// var modal = document.getElementById("myModal");
+
+// var btn = document.getElementById("myBtn");
+
+// var span = document.getElementsByClassName("close")[0];
+
+// $(btn).on("click", function () {
+// modal.style.display = "block";
+// })
+
+// $(span).on("click", function () {
+// modal.style.display = "none";
+// })
+
+// $(window).on("click", function () {
+// modal.style.display = "none";
+// })
+
+// // When the user clicks the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal
+
+//clicking anywhere on the screen when the lightbox is open should close the lightbox.
+
+
 //testimonials
 quoteOne = "Bonbon icing tiramisu ice cream candy";
 quoteTwo = "Carrot cake gingerbread jujubes sweet roll";
@@ -61,17 +99,19 @@ let quotesList = [quoteOne, quoteTwo, quoteThree];
 let p = 0;
 const quoteTime = 5000;
 
-function quoteChange(){
-$("#mytext").html(quotesList[p]);
-  if(p < quotesList.length - 1){
-    p++;
-  } else {
-    p = 0;
-  }
-  setTimeout("quoteChange()", quoteTime);
-}
+// function quoteChange(){
+// $("#mytext").html(quotesList[p]);
+//   if(p < quotesList.length - 1){
+//     p++;
+//   } else {
+//     p = 0;
+//   }
+//   setTimeout("quoteChange()", quoteTime);
+// }
 
-window.onload = quoteChange;
+// window.onload = quoteChange;
+
+
 
 
 
